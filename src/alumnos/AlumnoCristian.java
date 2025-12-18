746b37b7e34cff11120ccb47bb902e4bbf8233d9
cristicris <crimunpir@alu.edu.gva.es>
2025-12-18 12:41:19 +0100

    versionCristian

---
+++ src/alumnos/AlumnoCristian.java
@@ -0,0 +1,22 @@
+package alumnos;
+
+import java.util.ArrayList;
+
+
+public class AlumnoCristian {
+    public static ArrayList<Integer> misNumeros() {
+        int[] numeros= {77777, 65421, 73737, 33333, 95612,
+                30120, 12305, 51230, 33733, 77377};
+
+        ArrayList<Integer>  milista = new ArrayList<>(numeros.length);
+
+        for (int n : numeros) milista.add(n);
+
+        return  milista;
+
+    }
+
+    public static String nombre() {
+        return "Alfonso";   // o cualquier nombre que quieras mostrar
+    }
+}
