654237a359433a19f935c320f82b39aeffe3d79f
yaserr11 <yaserr11@alu.edu.gva.es>
2025-12-18 12:57:35 +0100

    la eleccion de yassir

--- src/alumnos/AlumnoYassir.java
+++ src/alumnos/AlumnoYassir.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class AlumnoYassir {
     public static ArrayList<Integer> misNumeros() {
         int[] numeros= {14897, 48372, 19506, 82714, 56039,
-                3148, 72941, 60823, 47195, 93268};
+                3148, 72941, 60823, 47195, 25017};
 
         ArrayList<Integer>  milista = new ArrayList<>(numeros.length);
 
